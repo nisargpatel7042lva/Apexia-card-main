db9ffda09b5a1ead21fd9be39873fb47bbad5258
Nisarg Patel <23csd11@svitvasad.ac.in>
2025-01-10 23:03:59 +0530

    initialize Next.js project with Tailwind CSS and basic layout

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import CardComponent from "@/components/card";
+
+export default function HomePage() {
+    return (
+        <div className="w-full h-screen flex justify-center items-center">
+            <CardComponent />
+        </div>
+    );
+}
